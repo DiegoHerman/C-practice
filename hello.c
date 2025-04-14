ecf00a3bd9ee2b5854b694ee4525b67e95d0a234
DiegoHerman <154596747+DiegoHerman@users.noreply.github.com>
2025-04-14 18:27:26 +0300

    Update hello.c

--- hello.c
+++ hello.c
@@ -3,6 +3,6 @@
 int main() {
     printf("Deez very very very big beautiful nuts");
     printf("\nDeez bigger nuts");
-    printf("\n i have zero hoes");
+    printf("\n i have zero hoes
     return 0;
 }
